47e1ff1adab3d1d9397ce8e755c99711f69db62b
duprof10 <duprofxadrez@yahoo.com.br>
2024-06-16 21:19:25 -0300

    commit novo

--- meu-primeiro-projeto-java/src/educ/eduardo/primeiraSemana/BoletimEstudantil.java
+++ meu-primeiro-projeto-java/src/educ/eduardo/primeiraSemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package educ.eduardo.primeiraSemana;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 7;
--- meu-primeiro-projeto-java/src/educ/eduardo/segundasemana/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/educ/eduardo/segundasemana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package educ.eduardo.segundasemana;
 public class MinhaClasse {
 
     public static void main(String[] args) {
